ca44cbb16faef3fe6a0e9a3bb7b67072294e425d
Monish.Trivedi <montriv111@yahoo.co.in>
2025-05-15 14:07:10 +0530

    Cross-browser

---
+++ src/main/java/config/BrowserFactory.java
@@ -0,0 +1,25 @@
+package config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BrowserFactory {
+    public static WebDriver getDriver(String browserName) {
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
+    }
+}
---
+++ src/main/java/tests/LoginTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import config.BrowserFactory;
+
+public class LoginTest {
+    WebDriver driver;
+    ExtentReports extent;
+    ExtentTest test;
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setup(String browser) {
+        ExtentSparkReporter spark = new ExtentSparkReporter("reports/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+        test = extent.createTest("Login Test on " + browser);
+
+        driver = BrowserFactory.getDriver(browser);
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        test.info("Navigated to Login Page");
+    }
+
+    @Test
+    public void testLogin() {
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+        test.info("Entered credentials and clicked login");
+        
+        driver.navigate().to("https://practicetestautomation.com/logged-in-successfully/");
+        Assert.assertEquals(driver.getTitle(), "Logged In Successfully | Practice Test Automation");
+        test.pass("Login successful");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            test.info("Browser closed");
+        }
+        extent.flush();
+    }
+}
