c6d2b940e1c98ebb9a6a77910c88ef53b00321b1
Monish.Trivedi <montriv111@yahoo.co.in>
2025-06-16 17:07:54 +0530

    Headless browser

--- src/main/java/config/BrowserFactory.java
+++ src/main/java/config/BrowserFactory.java
@@ -11,13 +11,17 @@ public class BrowserFactory {
         switch (browserName.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                return new ChromeDriver(chromeOptions);
 //            case "firefox":
 //                WebDriverManager.firefoxdriver().setup();
 //                return new FirefoxDriver();
             case "edge":
                 WebDriverManager.edgedriver().setup();
-                return new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+                return new EdgeDriver(edgeOptions);
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browserName);
         }
