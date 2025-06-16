729e43ed0d016670cb44ba286acdc078c4dffb52
Monish.Trivedi <montriv111@yahoo.co.in>
2025-06-16 17:12:31 +0530

    Headless browser

--- src/main/java/config/BrowserFactory.java
+++ src/main/java/config/BrowserFactory.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class BrowserFactory {
     public static WebDriver getDriver(String browserName) {
