e17871322b0f4921077ca8c9d9eb48246c328c95
Monish.Trivedi <montriv111@yahoo.co.in>
2025-06-20 12:31:01 +0530

    Cross-browser

--- src/main/java/config/BrowserFactory.java
+++ src/main/java/config/BrowserFactory.java
@@ -4,9 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeOptions;
+//import org.openqa.selenium.edge.EdgeOptions;
 
 public class BrowserFactory {
     public static WebDriver getDriver(String browserName) {
